a8ca61d5a4710e041d574d7cb9f467f1b9ba73af
Victor-Quqi <v1756251285@gmail.com>
2026-06-16 18:43:05 +0800

    chore: release plugin 0.1.8

--- plugin/scripts/lib/policy-core.mjs
+++ plugin/scripts/lib/policy-core.mjs
@@ -1,4 +1,4 @@
-export const PLUGIN_VERSION = "0.1.7";
+export const PLUGIN_VERSION = "0.1.8";
 export const TARGET_HOSTS = Object.freeze([
     "anyrouter.top",
     "a-ocnfniawgw.cn-shanghai.fcapp.run"
